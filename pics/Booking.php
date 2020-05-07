0f575598ce6f3ec8c9994f3a9cc4438498c3f15d
Sevinch19 <seva.durjanova@gmail.com>
2020-05-08 00:19:00 +0300

    all images and logos are added

---
+++ pics/Booking.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+ <style type="text/css">
+ 	
+ 	.class{
+ 		width: 500px;
+ 		height: 400px;
+ 		margin: 50px;
+ 		background-color: #FEBB02;
+ 	}
+
+ </style>
+
+</head>
+<body>
+
+<div class="book">
+	
+	<form class="form-part">
+		<input type="text" name="fname" placeholder="Enter your firstname">
+		<input type="text" name="sname" placeholder="Enter your second name">
+		<select>
+			<option value="1">1</option>
+			<option value="2">2</option>
+			<option value="3">3</option>
+			<option value="4">4</option>
+			<option value="5">5</option>
+			<option value="6">6</option>
+			<option value="7">7</option>
+			<option value="8">8</option>
+		</select>
+
+	</form>
+
+
+</div>
+
+</body>
+</html>
